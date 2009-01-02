59a0b342e90a874eb645b5faae9b18386097c0c7
Nicolas R Dufour <nrdufour@gmail.com>
2009-01-02 20:56:54 +0000

    Create a request class.

---
+++ src/info/embryosys/adt/manager/AdtOperation.java
@@ -0,0 +1,17 @@
+/*
+ * Embryo Systems
+ *
+ * $Id$
+ *
+ * Copyright 2009 Nicolas R. Dufour <nrdufour@gmail.com>
+ */
+package info.embryosys.adt.manager;
+
+/**
+ * @author nrdufour
+ * 
+ */
+public enum AdtOperation {
+
+	CREATE, HIBERN, AWAKE, DESTROY, RESUR, PURGE, RENAME,
+}
---
+++ src/info/embryosys/adt/manager/AdtRequest.java
@@ -0,0 +1,56 @@
+/*
+ * Embryo Systems
+ *
+ * $Id$
+ *
+ * Copyright 2009 Nicolas R. Dufour <nrdufour@gmail.com>
+ */
+package info.embryosys.adt.manager;
+
+import info.embryosys.adt.core.AdtType;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author nrdufour
+ * 
+ */
+public final class AdtRequest {
+
+	private final AdtOperation operation;
+	private final AdtType type;
+	private final List<String> arguments;
+
+	/**
+	 * 
+	 */
+	public AdtRequest(final AdtOperation op, AdtType type,
+			final List<String> args) {
+		this.operation = op;
+		this.type = type;
+		this.arguments = args;
+	}
+
+	/**
+	 * @return the operation
+	 */
+	public AdtOperation getOperation() {
+		return operation;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public AdtType getType() {
+		return type;
+	}
+
+	/**
+	 * @return the arguments
+	 */
+	public Iterator<String> getArguments() {
+		return arguments.iterator();
+	}
+
+}
