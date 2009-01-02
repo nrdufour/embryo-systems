1425c94d45dbb67a2c39daeb1f0a5303a36014e9
Nicolas R Dufour <nrdufour@gmail.com>
2009-01-02 20:00:05 +0000

    Some core classes (empty).

---
+++ src/info/embryosys/adt/core/AdtFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Embryo Systems
+ *
+ * $Id$
+ *
+ * Copyright 2009 Nicolas R. Dufour <nrdufour@gmail.com>
+ */
+package info.embryosys.adt.core;
+
+/**
+ * @author nrdufour
+ *
+ */
+public class AdtFactory {
+
+}
---
+++ src/info/embryosys/adt/manager/AdtManager.java
@@ -0,0 +1,16 @@
+/*
+ * Embryo Systems
+ *
+ * $Id$
+ *
+ * Copyright 2009 Nicolas R. Dufour <nrdufour@gmail.com>
+ */
+package info.embryosys.adt.manager;
+
+/**
+ * @author nrdufour
+ *
+ */
+public class AdtManager {
+
+}
