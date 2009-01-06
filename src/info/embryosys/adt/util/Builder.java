70fe68f5d4380b637aaf2124cf0ca67f76be9b66
Nicolas R Dufour <nrdufour@gmail.com>
2009-01-06 23:28:30 +0000

    Add the builder pattern.

---
+++ src/info/embryosys/adt/util/Builder.java
@@ -0,0 +1,18 @@
+/*
+ * Embryo Systems
+ *
+ * $Id$
+ *
+ * Copyright 2009 Nicolas R. Dufour <nrdufour@gmail.com>
+ * 
+ * This source code is released under the BSD License.
+ */
+package info.embryosys.adt.util;
+
+/**
+ * @author nrdufour
+ * 
+ */
+public interface Builder<T> {
+	T build();
+}
